f94352927677a4fa8344958e64a5983e7bf88170
ibrah <ibrahimhafez995@gmail.com>
2022-10-04 22:54:31 +0200

    second commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import json from '../';
+
+@Component({
+    selector: 'app-root',
+    encapsulation: ViewEncapsulation.None,
+    styleUrls: [
+        './app.component.css'
+    ],
+    template: `
+        <tree-diagram [data]="tree"></tree-diagram>
+    `
+})
+export class AppComponent implements OnInit {
+
+    public treeConfig = {
+        nodeWidth: 150,
+        nodeHeight: 100
+    };
+
+    public tree: any;
+
+
+    public ngOnInit() {
+        this.tree = {
+            json,
+            config: this.treeConfig
+        };
+    }
+
+}
